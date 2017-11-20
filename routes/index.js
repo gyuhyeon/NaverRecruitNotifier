1d83933deeeace7ce7d513c3785511ab54946ef1
Gyuhyeon <mark3236@naver.com>
2017-11-21 03:13:33 +0900

    added delete number functionality

--- routes/index.js
+++ routes/index.js
@@ -46,4 +46,31 @@ router.post('/enlist', function(req, res, next) {
     }); //end of request.post (sorry for callback hell!)
 }) //end of router post handling
 
+router.post('/unsubscribe', function(req, res, next) {
+    if(req.body['g-recaptcha-response'] === undefined || req.body['g-recaptcha-response'] === '' || req.body['g-recaptcha-response'] === null) {
+        return res.json({"response" : "Please complete recaptcha."});
+    }
+    var regex = /^\d{3}-\d{4}-\d{4}$/;
+    if(!req.body.phonenumber.match(regex)){
+        return res.json({"response" : "Please input a correct phone number. (000-0000-0000)"});
+    }
+    request.post({url:"https://www.google.com/recaptcha/api/siteverify", form:{"secret" : config.captchasecret, "response" : req.body['g-recaptcha-response']}}, function(error, response, body){
+        body = JSON.parse(body);
+        // Success will be true or false depending upon captcha validation.
+        if(body.success !== undefined && !body.success) {
+            return res.json({"response" : "Recaptcha validation failed, please try again."})
+        }
+        //everything OK, now we add the phone number to the DB.
+        connection.query('DELETE FROM `NaverJobs`.`NotifyList` WHERE `phonenumber`="'+req.body.phonenumber+'");', function(error, cursor){
+            if(error==null){
+                return res.json({"response" : "Success! Your number has been deleted."});
+            }
+            else{
+                return res.json({"response" : "We're sorry, but either our DB is not working, or your number is not in the database!"});
+            }
+        }); //end of insert connection.query
+
+    }); //end of request.post (sorry for callback hell!)
+});
+
 module.exports = router;
